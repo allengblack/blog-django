44b81a40a3948b1897860ea7176e820e75095cdf
allengblack <allengblack@gmail.com>
2016-10-08 15:56:37 +0100

    Added template for Poll details 08/10/16 15:56

--- polls/views.py
+++ polls/views.py
@@ -1,24 +1,24 @@
 
 from django.http import HttpResponse
 from .models import Question
-from django.template import loader
+# from django.template import loader
+from django.shortcuts import render, get_object_or_404
+# from django.http import Http404
 
 # Create your views here.
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')
-    # output = ' '.join(q.question_text for q in latest_question_list)
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list
     }
-    # return HttpResponse("You're at the Polls index page. <br>" + output)
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
